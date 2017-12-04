e51da8210e8d896b1d614644d4d9082011d00283
Leduc Axel <leduc.axel.pro@gmail.com>
2017-12-04 10:53:14 +0100

    Mon Autoloader si jamais on a pas le droit d'utiliser celui de composer

--- public/index.php
+++ public/index.php
@@ -1,5 +1,15 @@
 <?php
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
+// Autoload
+spl_autoload_register(function ($class) {
+    $split = explode('\\', $class);
+    $folder = strtolower($split[0]);
+    array_shift($split);
+    $file = $folder . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $split) . ".php";
+    require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . $file;
+});
+
+// Composer autoloader
+//require dirname(__DIR__) . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 
 $uri = isset($_GET['r']) ? $_GET['r'] : '/';
 $routes = require dirname(__DIR__) . DIRECTORY_SEPARATOR . "routes.php";
